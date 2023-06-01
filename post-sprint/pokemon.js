a307464d2fb18240a5389a1b1701bf836af7c636
LouKelly <louiskelly1999@gmail.com>
2023-06-01 14:50:13 +0100

    3 and 4 done

--- post-sprint/pokemon.js
+++ post-sprint/pokemon.js
@@ -1,25 +1,112 @@
 class Pokemon {
-    constructor(name, hitPoints, attackDamage, move = 'tackle') {
-        this.name = name;
-        this.hitPoints = hitPoints;
-        this.attackDamage = attackDamage;
-        this.move = move
+  constructor(name, hitPoints, attackDamage, move = "Tackle") {
+    this.name = name;
+    this.hitPoints = hitPoints;
+    this.attackDamage = attackDamage;
+    this.move = move;
+  }
+  takeDamage(num) {
+    this.hitPoints -= num;
+  }
+  useMove() {
+    console.log(`${this.name} used ${this.move}`);
+    return this.attackDamage;
+  }
+  hasFainted() {
+    if (this.hitPoints > 0) {
+      return false;
     }
-    takeDamage(num) {
-     this.hitPoints -= num
+    return true;
+  }
+  isEffectiveAgainst(pokemon) {
+    if (this.type === "Fire" && pokemon.type === "Grass") {
+      return true;
+    } else if (this.type === "Water" && pokemon.type === "Fire") {
+      return true;
+    } else if (this.type === "Grass" && pokemon.type === "Water") {
+      return true;
     }
-    useMove() {
-    console.log(`${this.name} used ${this.move}`)
-    return this.attackDamage
+    return false;
+  }
+  isWeakTo(pokemon) {
+    if (this.type === "Fire" && pokemon.type === "Water") {
+      return true;
+    } else if (this.type === "Water" && pokemon.type === "Grass") {
+      return true;
+    } else if (this.type === "Grass" && pokemon.type === "Fire") {
+      return true;
     }
-    hasFainted() {
-        if(this.hitPoints > 0) {
-            return false 
-        }
-        return true
-    }
+    return false;
+  }
 }
 
+class FireType extends Pokemon {
+  constructor(name, hitPoints, attackDamage, move) {
+    super(name, hitPoints, attackDamage, move);
+    this.type = "Fire";
+  }
+}
 
+class WaterType extends Pokemon {
+  constructor(name, hitPoints, attackDamage, move) {
+    super(name, hitPoints, attackDamage, move);
+    this.type = "Water";
+  }
+}
 
-module.exports = Pokemon
+class GrassType extends Pokemon {
+  constructor(name, hitPoints, attackDamage, move) {
+    super(name, hitPoints, attackDamage, move);
+    this.type = "Grass";
+  }
+}
+
+class NormalType extends Pokemon {
+  constructor(name, hitPoints, attackDamage, move) {
+    super(name, hitPoints, attackDamage, move);
+    this.type = "Normal";
+  }
+}
+
+class Charmander extends FireType {
+  constructor(name, hitPoints, attackDamage, move) {
+    super(name, hitPoints, attackDamage, move);
+    this.name = "Charmander";
+    this.move = "Ember";
+  }
+}
+
+class Squirtle extends WaterType {
+  constructor(name, hitPoints, attackDamage, move) {
+    super(name, hitPoints, attackDamage, move);
+    this.name = "Squirtle";
+    this.move = "Water Gun";
+  }
+}
+
+class Bulbasaur extends GrassType {
+  constructor(name, hitPoints, attackDamage, move) {
+    super(name, hitPoints, attackDamage, move);
+    this.name = "Bulbasaur";
+    this.move = "Vine Whip";
+  }
+}
+
+class Rattata extends NormalType {
+  constructor(name, hitPoints, attackDamage, move) {
+    super(name, hitPoints, attackDamage, move);
+    this.name = "Rattata";
+  }
+}
+
+module.exports = {
+  Pokemon,
+  FireType,
+  WaterType,
+  GrassType,
+  NormalType,
+  Charmander,
+  Squirtle,
+  Bulbasaur,
+  Rattata,
+};
--- post-sprint/pokemon.test.js
+++ post-sprint/pokemon.test.js
@@ -1,34 +1,130 @@
-const Pokemon = require("./pokemon")
+const {
+  Pokemon,
+  FireType,
+  WaterType,
+  GrassType,
+  NormalType,
+  Charmander,
+  Squirtle,
+  Bulbasaur,
+  Rattata,
+} = require("./pokemon");
 
-describe('pokemon', () => {
-   test('should return a string of function', () => {
- expect(typeof Pokemon).toBe('function')
-});
-test('returns a pokemon object with a default move of tackle', () => {
-    const newPokemon = new Pokemon('Pikachu', 100, 20)
-    console.log(newPokemon)
-    expect(newPokemon.move).toBe('tackle')
-});
-test('should take a number and reduce its health by the number given', () => {
-    const newPokemon = new Pokemon('Pikachu', 100, 20)
-   newPokemon.takeDamage(50)
-   console.log(newPokemon)
-   expect(newPokemon.hitPoints).toBe(50)
-});
-test('should return the Pokemons attackDamage and console log pokemons move', () => {
-    const newPokemon = new Pokemon('Pikachu', 100, 20, 'headbutt')
-   const actual = newPokemon.useMove() 
-    console.log(newPokemon)
-    expect(actual).toBe(20)
-});
-test('hasFainted will return a boolean based on whether the Pokemon has fainted', () => {
-    const newPokemon = new Pokemon('Pikachu', 100, 20, 'headbutt')
-    let actual = newPokemon.hasFainted()
-    expect(actual).toBe(false)
-    newPokemon.takeDamage(150)
-    const newActual = newPokemon.hasFainted()
-    console.log(actual)
-    expect(newActual).toBe(true)
-});
+describe("pokemon", () => {
+  test("should return a string of function", () => {
+    expect(typeof Pokemon).toBe("function");
+  });
+  test("returns a pokemon object with a default move of tackle", () => {
+    const newPokemon = new Pokemon("Pikachu", 100, 20);
+    expect(newPokemon.move).toBe("Tackle");
+  });
+  test("should take a number and reduce its health by the number given", () => {
+    const newPokemon = new Pokemon("Pikachu", 100, 20);
+    newPokemon.takeDamage(50);
+    expect(newPokemon.hitPoints).toBe(50);
+  });
+  test("should return the Pokemons attackDamage and console log pokemons move", () => {
+    const newPokemon = new Pokemon("Pikachu", 100, 20, "headbutt");
+    const actual = newPokemon.useMove();
+    expect(actual).toBe(20);
+  });
+  test("hasFainted will return a boolean based on whether the Pokemon has fainted", () => {
+    //arrange
+    const newPokemon = new Pokemon("Pikachu", 100, 20, "headbutt");
+    //act
+    let actual = newPokemon.hasFainted();
+    //assert
+    expect(actual).toBe(false);
+    //act
+    newPokemon.takeDamage(150);
+    const newActual = newPokemon.hasFainted();
+    //assert
+    expect(newActual).toBe(true);
+  });
+  test("Checking new types", () => {
+    //arrange + act
+    const firePokemon = new FireType("Flame", 80, 25, "Fire");
+    const waterPokemon = new WaterType("Water", 80, 25, "Water");
+    const grassPokemon = new GrassType("Earth", 80, 25, "Grass");
+    const normalPokemon = new NormalType("Normal", 80, 25, "Normal");
+    //assert
+    expect(firePokemon.type).toBe("Fire");
+    expect(waterPokemon.type).toBe("Water");
+    expect(grassPokemon.type).toBe("Grass");
+    expect(normalPokemon.type).toBe("Normal");
+  });
+  test("Checking new types", () => {
+    //arrange + act
+    const firePokemon = new FireType("Flame", 80, 25, "Fire");
+    const waterPokemon = new WaterType("Water", 80, 25, "Water");
+    const grassPokemon = new GrassType("Earth", 80, 25, "Grass");
+    const normalPokemon = new NormalType("Normal", 80, 25, "Normal");
 
+    //assert - fire
+    expect(firePokemon).toBeInstanceOf(Pokemon);
+
+    expect(firePokemon.isEffectiveAgainst(grassPokemon)).toBe(true);
+    expect(firePokemon.isEffectiveAgainst(normalPokemon)).toBe(false);
+
+    expect(firePokemon.isWeakTo(waterPokemon)).toBe(true);
+    expect(firePokemon.isWeakTo(normalPokemon)).toBe(false);
+
+    //assert - water
+    expect(waterPokemon).toBeInstanceOf(Pokemon);
+
+    expect(waterPokemon.isEffectiveAgainst(firePokemon)).toBe(true);
+    expect(waterPokemon.isEffectiveAgainst(normalPokemon)).toBe(false);
+
+    expect(waterPokemon.isWeakTo(grassPokemon)).toBe(true);
+    expect(waterPokemon.isWeakTo(normalPokemon)).toBe(false);
+
+    //assert - grass
+    expect(grassPokemon).toBeInstanceOf(Pokemon);
+
+    expect(grassPokemon.isEffectiveAgainst(waterPokemon)).toBe(true);
+    expect(grassPokemon.isEffectiveAgainst(normalPokemon)).toBe(false);
+
+    expect(grassPokemon.isWeakTo(firePokemon)).toBe(true);
+    expect(grassPokemon.isWeakTo(normalPokemon)).toBe(false);
+
+    //assert - normal
+    expect(normalPokemon).toBeInstanceOf(Pokemon);
+
+    expect(normalPokemon.isEffectiveAgainst(grassPokemon)).toBe(false);
+    expect(normalPokemon.isEffectiveAgainst(normalPokemon)).toBe(false);
+
+    expect(normalPokemon.isWeakTo(waterPokemon)).toBe(false);
+    expect(normalPokemon.isWeakTo(normalPokemon)).toBe(false);
+  });
+  test("Checking new types", () => {
+    //arrange + act
+    const charmander = new Charmander("", 30, 15);
+    const squirtle = new Squirtle("", 45, 10);
+    const bulbasaur = new Bulbasaur("", 45, 10);
+    const rattata = new Rattata("", 45, 10);
+
+    //assert - Charmander
+    expect(charmander).toBeInstanceOf(FireType);
+    expect(charmander).toBeInstanceOf(Pokemon);
+    expect(charmander.type).toBe("Fire");
+    expect(charmander.move).toBe("Ember");
+
+    //assert - Squirtle
+    expect(squirtle).toBeInstanceOf(WaterType);
+    expect(squirtle).toBeInstanceOf(Pokemon);
+    expect(squirtle.type).toBe("Water");
+    expect(squirtle.move).toBe("Water Gun");
+
+    //assert - Bubasaur
+    expect(bulbasaur).toBeInstanceOf(GrassType);
+    expect(bulbasaur).toBeInstanceOf(Pokemon);
+    expect(bulbasaur.type).toBe("Grass");
+    expect(bulbasaur.move).toBe("Vine Whip");
+
+    //assert - Rattata
+    expect(rattata).toBeInstanceOf(NormalType);
+    expect(rattata).toBeInstanceOf(Pokemon);
+    expect(rattata.type).toBe("Normal");
+    expect(rattata.move).toBe("Tackle");
+  });
 });
